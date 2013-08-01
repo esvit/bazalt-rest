bf24386586c5941a988c5bd83920739eeeab3680
Vitalii Savchuk <esvit666@gmail.com>
2013-08-02 00:05:01 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+bazalt-orm
+==========
+
+[![Build Status](https://travis-ci.org/esvit/bazalt-orm.png)](https://travis-ci.org/esvit/bazalt-orm) [![Coverage Status](https://coveralls.io/repos/esvit/bazalt-orm/badge.png?branch=master)](https://coveralls.io/r/esvit/bazalt-orm?branch=master)
+
+Simple ORM on PHP
---
+++ src/Bazalt/Rest/Response.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bazalt\Rest;
+
+class Response extends \Tonic\Resource
+{
+    /**
+     * Condition method to turn output into JSON
+     */
+    protected function json()
+    {
+        $this->before(function ($request) {
+            if ($request->contentType == "application/json") {
+                $request->data = json_decode($request->data);
+            }
+        });
+        $this->after(function ($response) {
+            $response->contentType = "application/json";
+
+            if (isset($_GET['jsonp'])) {
+                $response->body = $_GET['jsonp'] . '(' . json_encode($response->body) . ');';
+            } else {
+                $response->body = json_encode($response->body);
+            }
+        });
+    }
+}
