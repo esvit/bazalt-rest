dbe7f0e8832c558673f1dceea95fd44c6e91af7a
Vitalii Savchuk <esvit666@gmail.com>
2013-08-02 00:18:45 +0300

    rename

--- src/Bazalt/Rest/Response.php
+++ src/Bazalt/Rest/Response.php
@@ -2,26 +2,6 @@
 
 namespace Bazalt\Rest;
 
-class Response extends \Tonic\Resource
+class Response extends \Tonic\Response
 {
-    /**
-     * Condition method to turn output into JSON
-     */
-    protected function json()
-    {
-        $this->before(function ($request) {
-            if ($request->contentType == "application/json") {
-                $request->data = json_decode($request->data);
-            }
-        });
-        $this->after(function ($response) {
-            $response->contentType = "application/json";
-
-            if (isset($_GET['jsonp'])) {
-                $response->body = $_GET['jsonp'] . '(' . json_encode($response->body) . ');';
-            } else {
-                $response->body = json_encode($response->body);
-            }
-        });
-    }
 }
