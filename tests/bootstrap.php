9b9e32fdc240719f89f1ca6d0c216f5abf812bd5
Alex Slubsky <aslubsky@gmail.com>
2013-09-13 14:57:39 +0300

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,14 +7,3 @@ require_once (is_file(__DIR__ . '/../vendor/autoload.php') ? (__DIR__ . '/../ven
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->add('tests', __DIR__ . '/..');
 $loader->register();
-
-$dbParams = array(
-    'server' => $GLOBALS['db_host'],
-    'username' => $GLOBALS['db_username'],
-    'password' => $GLOBALS['db_password'],
-    'database' => $GLOBALS['db_name'],
-    'port' => $GLOBALS['db_port']
-);
-
-$connectionString = new \Bazalt\ORM\Adapter\Mysql($dbParams);
-\Bazalt\ORM\Connection\Manager::add($connectionString, 'default');
