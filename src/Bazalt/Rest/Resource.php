f1a966d2ca3f83e19ee0780e30092359d75898cd
Vitalii Savchuk <esvit666@gmail.com>
2013-11-25 20:29:11 +0200

    add multiple params

--- src/Bazalt/Rest/Resource.php
+++ src/Bazalt/Rest/Resource.php
@@ -4,6 +4,29 @@ namespace Bazalt\Rest;
 
 class Resource extends \Tonic\Resource
 {
+    public static function params()
+    {
+        $query = $_SERVER['QUERY_STRING'];
+        $vars = array();
+        $second = array();
+        foreach (explode('&', $query) as $pair) {
+            list($key, $value) = explode('=', $pair);
+            if ('' == trim($value)) {
+                continue;
+            }
+
+            if (array_key_exists($key, $vars)) {
+                if (!array_key_exists($key, $second)) {
+                    $second[$key] = array($vars[$key]);
+                }
+                $second[$key][] = $value;
+            } else {
+                $vars[$key] = urldecode($value);
+            }
+        }
+        return array_merge($vars, $second);
+    }
+
     /**
      * Condition method to turn output into JSON
      */
--- tests/ResourceTest.php
+++ tests/ResourceTest.php
@@ -25,6 +25,26 @@ class ResourceTest extends \Bazalt\Rest\Test\BaseCase
     {
     }
 
+    public function testParams()
+    {
+        $_SERVER['QUERY_STRING'] = 'param1=2549&appname=appName1&appname=appName2&appname=appName3&appname=appName4&appname=appName5&apptype=thetype&idsess=1231324567980147dzeze55sd4&action=myaction';
+
+        $this->assertEquals(array (
+            'param1' => '2549',
+            'appname' =>
+                array (
+                    0 => 'appName1',
+                    1 => 'appName2',
+                    2 => 'appName3',
+                    3 => 'appName4',
+                    4 => 'appName5',
+                ),
+            'apptype' => 'thetype',
+            'idsess' => '1231324567980147dzeze55sd4',
+            'action' => 'myaction',
+        ), \Bazalt\Rest\Resource::params());
+    }
+
     public function testGet()
     {
         $response = new \Bazalt\Rest\Response(200, array(
